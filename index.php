926ad0d4c8f12cb9bd371cd7fb035013e98949af
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-06-12 14:21:19 +0000

    add all

--- index.php
+++ index.php
@@ -64,6 +64,110 @@ switch ($etudes){
   default :
   echo "vous n'avez pas été loin dans vos études!";
 }
+echo "<br>";
+
+// La différence entre "switch " et " match":
+switch(2) {
+  case 0 : 
+  $result = 'Paul';
+  break;
+  case 1 : 
+  $result = 'Marie';
+  break;
+  case 2 :
+  $result = 'Jean';
+}
+echo $result;
+
+echo "<br>";
+// la meme instruction avec match :
+echo match (2) {
+  0 => 'Paul',
+  1 => 'Marie',
+  2 => 'Jean',
+};
+
+echo "<br>";
+// Autre exemple avec l'instruction "match" :
+
+echo match (5) {
+  0 => 'continuer à travailler ',
+  1 => 'faire les travaux',
+  2 => 's\occuper des enfants',
+  3 => 'aider les amis ',
+  4 => 'partir en vacances',
+  default => 'je ne sais pas quoi faire !'
+};
+echo "<br>";
+
+//      Autre exemple de  "switch" && "match" :
+
+//    switch:
+$running = "Michel";
+ switch ($running) {
+   case "Paul":
+   echo "Le vainqueur est Paul";
+   break ;
+
+   case "Michel":
+   echo "le vainqueur est Michel";
+   break;
+
+   case "Pauline":
+   echo "Le vainqueur est Pauline";
+   break;
+
+   default :
+   echo "Nous ne connaissons pas le vainqueur de la course :";
+ }
+echo "<br>";
+
+// la meme avce Match:
+$running = "Paul";
+
+echo match($running) {
+  "Paul" => "le gagnant est Paul",
+  "Michel" => "le gagnant est Michel",
+  "Pauline" => " Le gagnant est Pauline",
+  default => " Nous ne connaissons pas le gagnant", 
+  
+};
+echo "<br>";
+
+// example switch :
+$day = "Jeudi";
+switch($day) {
+  case "Lundi":
+  echo "Aujourd'hui c'est Lundi !";
+  break;
+
+  case "Mardi":
+  echo "Aujourd'hui c'est Mardi !";
+  break;
+
+  case "Mercredi":
+  echo "Aujourd'hui c'est Mercredi !";
+  break;
+
+  case "Jeudi":
+  echo "Aujourd'hui c'est Jeudi !";
+  break;
+
+  case "Vendredi":
+  echo "Aujourd'hui c'est Vendredi !";
+  break;
+
+  case "Samedi":
+  echo "Aujourd'hui c'est Samedi !";
+  break;
+
+  case "Dimanche":
+  echo "Aujourd'hui c'est Dimanche !";
+  break;
+  
+  default:
+  echo "nous ne connaissons pas le jour d'aujourd'hui !";
+}
 
 ?> 
 
